b3a39b41c5e38df29fab8d44fea981a327a2377f
ekosakti <ekosakti@ub.ac.id>
2025-10-27 12:13:15 +0700

    Update local_consumer.py

--- local_consumer/local_consumer.py
+++ local_consumer/local_consumer.py
@@ -43,7 +43,7 @@ class LocalApacheLogConsumer:
                 KAFKA_TOPIC,
                 bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
                 group_id=KAFKA_GROUP_ID,
-                auto_offset_reset='latest',  # Hanya baca message baru
+                auto_offset_reset='latest',  # Hanya baca message baru #baca dari awal 'earliest'
                 enable_auto_commit=True,
                 value_deserializer=lambda m: json.loads(m.decode('utf-8')),
                 key_deserializer=lambda k: k.decode('utf-8') if k else None
